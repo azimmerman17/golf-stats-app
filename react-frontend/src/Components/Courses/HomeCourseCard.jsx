17ee80b51f6031f55f1faf4318243d9625757566
Andrew Zimmerman <azimmerman@pga.com>
2025-02-25 05:04:14 -0500

    connected home facility card to facility page

--- react-frontend/src/Components/Courses/HomeCourseCard.jsx
+++ react-frontend/src/Components/Courses/HomeCourseCard.jsx
@@ -4,16 +4,26 @@ import Card from 'react-bootstrap/Card';
 import FacilityDetails from './FacilityDetails';
 
 import { CurrentPage } from '../../Contexts/CurrentPageContext';
+import { CurrentFacility } from '../../Contexts/CurrentFacilityContext';
 
 const HomeCourseCard = ({ facility, active }) => {
   const {currentPage, setCurrentPage} = useContext(CurrentPage)
+  const {currentFacility, setCurrentFacility} = useContext(CurrentFacility)
+
+  const handleClick = () => {
+    if (facility) {
+      const { FACILITY_ID } = facility
+      setCurrentFacility(FACILITY_ID)
+      setCurrentPage('Facility')
+    }
+  }
 
   return (
-    <Button variant='danger' className='p-1 my-1 text-center shadow-lg' disabled={!active} onClick={e => setCurrentPage('Home Course')}>
+    <Button variant='danger' className='p-1 my-1 text-center shadow-lg' disabled={!active} onClick={e => handleClick()}>
       <Card className='m-0 p-0 w-100 h-100'>
         <Card.Body>
           <Card.Title className='text-center'>{facility ? facility.NAME : null}</Card.Title>
-          <Card.Subtitle className="mb-2 text-muted"><small>{facility ? 'HOME COURSE' : null}</small></Card.Subtitle>
+          <Card.Subtitle className='mb-2 text-muted'><small>{facility ? 'HOME COURSE' : null}</small></Card.Subtitle>
           {facility ? <FacilityDetails facility={facility} /> : (
             <Card.Text>
               No home course is set, click to set a home course
